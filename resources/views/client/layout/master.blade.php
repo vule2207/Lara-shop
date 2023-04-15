2aee72d9c559bc326c6038b2086c12c4b03aebde
Le Thanh Vu <levu2207@hanbiro.vn>
2023-04-15 13:44:57 +0700

    update

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -15,15 +15,15 @@
 
     <!-- Css Styles -->
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css">
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/themify-icons.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/themify-icons.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="https://lara-shop.vercel.app/assets/css/style.css" type="text/css">
 </head>
 
 <body>
